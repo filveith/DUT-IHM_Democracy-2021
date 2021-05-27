dcd224185033a238a34dc704b9bdccdb161a38ee
fveith <fveith@etu.u-bordeaux.fr>
2021-05-27 09:58:14 +0200

    all files

---
+++ BaseSim2021/BaseSim2021/GameView.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaseSim2021
+{
+    public partial class GameView : Form
+    {
+        private readonly WorldState theWorld;
+        /// <summary>
+        /// The constructor for the main window
+        /// </summary>
+        public GameView(WorldState world)
+        {
+            InitializeComponent();
+            theWorld = world;
+        }
+        /// <summary>
+        /// Method called by the controler whenever some text should be displayed
+        /// </summary>
+        /// <param name="s"></param>
+        public void WriteLine(string s)
+        {
+            List<string> strs = s.Split('\n').ToList();
+            strs.ForEach(str=>outputListBox.Items.Add(str));
+            if (outputListBox.Items.Count > 0)
+            {
+                outputListBox.SelectedIndex = outputListBox.Items.Count - 1;
+            }
+            outputListBox.Refresh();
+        }
+        /// <summary>
+        /// Method called by the controler whenever a confirmation should be asked
+        /// </summary>
+        /// <returns>Yes iff confirmed</returns>
+        public bool ConfirmDialog()
+        {
+            string message = "Confirmer ?";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            return MessageBox.Show(message, "", buttons) == DialogResult.Yes;
+        }
+        #region Event handling
+        private void InputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Enter))
+            {
+                e.SuppressKeyPress = true; // Or beep.
+                GameController.Interpret(inputTextBox.Text);
+            }
+        }
+
+        private void GameView_Paint(object sender, PaintEventArgs e)
+        {
+            getAllValues(e);
+            //theWorld.Perks.ForEach(v => new IndexedValueView(v, Width / 2, Height / 2, Width / 4, Height / 4).Draw(e.Graphics));
+            //theWorld.Taxes.ForEach(v => new IndexedValueView(v, 20, 20, Width / 4, Height / 4).Draw(e.Graphics));
+            diffLabel.Text = "Difficulté : " + theWorld.TheDifficulty;
+            turnLabel.Text = "Tour " + theWorld.Turns;
+            moneyLabel.Text = "Trésor : " + theWorld.Money + " pièces d'or";
+            gloryLabel.Text = "Gloire : " + theWorld.Glory;
+            nextButton.Visible = true;
+        }
+        #endregion
+
+        private void printValues(PaintEventArgs e, List<IndexedValue> values, int x, int y, Size recSize, Color color)
+        {
+            int margin = 25;
+
+            recSize = new Size(recSize.Width - margin*3, recSize.Height);
+
+            int nbValue = values.Count();
+            int baseX = x+margin;
+            int baseY = y;
+            int increment = 0;
+            int width = 50;
+            int height = 50;
+            
+
+            //Console.WriteLine(nbValue.ToString(), color.GetType().ToString());
+            int nbInRow = recSize.Width / width;
+            int reste = recSize.Width - nbValue * width;
+            int espacementX = reste / (nbValue + 1);
+            Pen pen = new Pen(color, 2);
+            List<IndexedValueView> views = new List<IndexedValueView>();
+
+            
+            e.Graphics.DrawRectangle(pen, new Rectangle(new Point(x+margin, y+margin), recSize));
+
+            y = baseY;
+            x = baseX;
+
+            foreach (IndexedValue v in values)
+            {
+                if (x + width + 5 >= recSize.Width)
+                {
+                    Console.WriteLine(color+"    To width = " + (x + width + 5) + "   over = " + recSize.Width);
+                    y += width + 5;
+                    increment = 0;
+                    x = baseX;
+                }
+
+                
+
+                increment++;
+                x += espacementX + width;
+                Console.WriteLine("x = " + x + "    y = " + y);
+                //views.Add(new IndexedValueView(v, x, y + recSize.Height / 2 - height / 2, width, height, color));
+                views.Add(new IndexedValueView(v, x, y + width - 15, width, height, color));
+                x += width;
+            }
+            views.ForEach(view => view.Draw(e.Graphics));
+        }
+
+        private void getAllValues(PaintEventArgs e)
+        {
+            printValues(e, theWorld.Policies, 0, 0, new Size(Width, Height / 3), Color.FromName("Blue"));
+
+            printValues(e, theWorld.Crises, 0, 25 + Height - (Height / 3) * 2, new Size(Width / 2, Height - (Height / 3)*2), Color.FromName("Red"));
+            printValues(e, theWorld.Indicators, Width/2, 25 + Height - (Height / 3) * 2, new Size(Width / 2, Height - (Height / 3) * 2), Color.FromName("Green"));
+            
+            printValues(e, theWorld.Groups, 0, 25 + Height - (Height / 3), new Size(Width / 2, -100 + Height - (Height / 3) * 2), Color.FromName("Pink"));
+            printValues(e, theWorld.Perks, Width / 2, 25 + Height - (Height / 3), new Size(Width / 2, -100 + Height - (Height / 3) * 2), Color.FromName("Orange"));
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            GameController.Interpret("suivant");
+        }
+
+    }
+}
---
+++ BaseSim2021/BaseSim2021/IndexedValueView.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BaseSim2021
+{
+    class IndexedValueView
+    {
+        public IndexedValue Value { get; set; }
+        public Point Position { get; set; }
+        public Size Taille { get; set; }
+        public int Epaisseur { get; set; } = 1;
+        public Color Couleur { get; set; } = Color.FromName("Green");
+        public String Texte { get; set; } = "";
+
+        //public Point Centre
+        //{
+            //get
+            //{
+            //    return new Point(Position.X + Taille.Width / 2,
+              //                   Position.Y + Taille.Height / 2);
+          //  }
+        //}
+
+        public IndexedValueView(IndexedValue value_, int x, int y, int sx, int sy, Color c)
+        {
+            Value = value_;
+            Position = new Point(x, y);
+            Taille = new Size(sx, sy);
+            Couleur = c;
+        }
+
+        public void Draw(Graphics g)
+        {
+            Pen p = new Pen(Couleur, Epaisseur);
+            
+            Rectangle r = new Rectangle(this.Position, Taille);
+
+            g.DrawRectangle(p, r);
+
+            g.DrawString(Value.Type.ToString(), new Font("Times New Roman", 10, FontStyle.Bold), Brushes.Black, this.Position);
+
+            g.DrawString(Value.Name, new Font("Times New Roman", 8, FontStyle.Bold), Brushes.Black, new Point(this.Position.X, this.Position.Y+11));
+
+            g.DrawString(Value.Value.ToString(), new Font("Times New Roman", 7, FontStyle.Bold), Brushes.Black, new Point(this.Position.X+20, this.Position.Y + 20));
+
+            g.DrawString(Value.MinValue.ToString(), new Font("Times New Roman", 6, FontStyle.Bold), Brushes.Black, new Point(this.Position.X, this.Position.Y + 20));
+
+            g.DrawString(Value.MaxValue.ToString(), new Font("Times New Roman", 6, FontStyle.Bold), Brushes.Black, new Point(this.Position.X+40, this.Position.Y + 20));
+
+        }
+
+        public bool Contient(Point p)
+        {
+            Rectangle r = new Rectangle(new Point(Position.X - Taille.Width / 2, Position.Y - Taille.Height / 2), Taille);
+            return r.Contains(p);
+        }
+
+        public Point Deplace(Point p)
+        {
+            int x = p.X;
+            int y = p.Y;
+            return new Point(x, y);
+        }
+        public void DessineSouris(Graphics g, Point posM)
+        {
+            g.DrawLine(Pens.Blue, Position, posM);
+        }
+    }
+}
